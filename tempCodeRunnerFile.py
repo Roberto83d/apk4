21077c0158303c8749279628fcfc77a7288be5a5
Roberto83d <143013856+Roberto83d@users.noreply.github.com>
2023-12-26 18:26:15 +0100

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # Upewnij się, że nazwa pliku szablonu jest poprawna
+    return render_template('index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
